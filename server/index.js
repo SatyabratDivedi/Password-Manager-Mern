fc0f80155c170e1a4c3d9928815723127193bcda
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-03-29 18:49:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 dotenv.config();
 app.use(cors({
-    origin:'http://localhost:5173'
+    origin:'https://password-manager-mern.vercel.app/'
 }))
 
 const PORT = process.env.PORT || 3000;
